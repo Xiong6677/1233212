7c16d39809fa0a17640afeca13b5714c76668fbc
AndrewSunStrike <mrperck@gmail.com>
2025-05-13 16:40:36 +0300

    Add files via upload

---
+++ ConsoleApplication5.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "");
+
+	double Q, W, t, S, n, H, Hr, h, Hc, Re, L, d, V, g, ny, ld;
+
+	// Ввод данных
+	cout << "Введите t: ";
+	cin >> t;
+	cout << "Введите S: ";
+	cin >> S;
+	cout << "Введите n: ";
+	cin >> n;
+	cout << "Введите Hr: ";
+	cin >> Hr;
+	cout << "Введите L: ";
+	cin >> L;
+	cout << "Введите d: ";
+	cin >> d;
+
+	// Константы
+	g = 9.8;
+	V = 1.0; // Предполагаемая скорость потока, м/с
+	ny = 1e-6;
+	Hc = 5;
