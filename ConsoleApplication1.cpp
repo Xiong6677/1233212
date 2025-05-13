442cc4a904bf071915ea31c1173d338582aff8a0
Xiong6677 <danillelyokin@yandex.ru>
2025-05-13 16:38:40 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,21 @@
+﻿// Расчеты
+W = 0.02 * S + 0.05 * n;
+Q = W / t;
+
+Re = (d * V) / ny;
+
+if (Re < 2300) {
+	ld = 64 / Re;
+}
+else {
+	ld = 0.3 / pow(Re, 0.25);
+}
+
+h = ld * (L / d) + (V * V) / (2 * g);
+H = Hr + h + Hc;
+
+// Вывод
+cout << "Вывод Q: " << Q << endl;
+cout << "Вывод H: " << H << endl;
+
+return 0;
